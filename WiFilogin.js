5a9674a6cdb1964ae25788ff4e1d0654c6936654
aichiyude27 <xiao6250000@126.com>
2018-06-21 13:41:12 +0800

    0.3

--- WiFilogin.js
+++ WiFilogin.js
@@ -2,8 +2,8 @@
 // @name 
 // @name:en      ShanghaiTech WiFi Auto login
 // @name           跳过网站等待、验证码及登录
-// @namespace http://tampermonkey.net/
-// @version 0.2
+// @namespace https://github.com/aichiyu/Auto-login-wifi-with-tampermonkey
+// @version 0.3
 // @description auto login shanghaitech WiFi
 // @author 爱吃鱼的27
 // @match *controller.shanghaitech.edu.cn*
